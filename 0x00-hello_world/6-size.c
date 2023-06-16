15adf820998a7a75a32a081042ac729b24ee26ec
Mohamed Riad <mohamedreyad8811@gmail.com>
2023-06-15 18:28:04 -0700

    last

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <sudio.h>
 /**
 * main - Entry Point to sizes
 *
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-printf ("This is Size of a char : %d byte(s)\n", sizeof(char));
-printf ("This is Size of a int : %d byte(s)\n", sizeof(int));
-printf ("This is Size of a long int : %d byte(s)\n", sizeof(long int));
-printf ("This is Size of a long long int : %d byte(s)\n", sizeof(long long int));
-printf ("This is Size of a float : %d byte(s)\n", sizeof(float));
+printf("Size of a char : %d byte(s)\n", sizeof(char));
+printf("Size of an int : %d byte(s)\n", sizeof(int));
+printf("Size of a long int : %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int : %d byte(s)\n", sizeof(long long int));
+printf("Size of a float : %d byte(s)\n", sizeof(float));
 return (0);
 }
