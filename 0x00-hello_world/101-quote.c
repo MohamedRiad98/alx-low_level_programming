c121338f5b5ac9ecd868e9fff64ad7b73a1086e0
Mohamed Riad <mohamedreyad8811@gmail.com>
2023-06-15 19:07:51 -0700

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Prints out the last part of a quote in the standard error.
+ * main - Print quote in the standard error.
  * Return: 1 if success.
  */
-	int main(void)
-			{
-       				 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-				 return (1);
-			}
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}			}
