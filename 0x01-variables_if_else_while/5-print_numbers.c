b1e5445b953feb57e430fb213f92436de872ac94
Mohamed Riad <mohamedreyad8811@gmail.com>
2023-06-18 21:56:48 -0700

    l

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,9 +9,10 @@ int main(void)
 {
 	int number;
 
-	for (number = 0;number <= 9;number++)
+	for (number = 0; number <= 9; number++)
 	{
 			printf("%d", number);
+			printf("\n")
 
 	}
 	return (0);
