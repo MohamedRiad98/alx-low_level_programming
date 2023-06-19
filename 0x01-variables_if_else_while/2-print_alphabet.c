3a961446fea81f5dce46b21d765a63636506942a
Mohamed Riad <mohamedreyad8811@gmail.com>
2023-06-18 20:06:45 -0700

    l

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,8 +8,6 @@
 int main() {
     char letter;
     for (letter = 'a'; letter <='z';letter++){
-        putchar(letter);
-    }
+        putchar(letter);}
     putchar('\n');
-    return 0;
-}
+    return 0;}
