c592b49ea10ef4d61607a304f41bfcbdaf4290be
root <root@MohammedPC>
2023-06-19 20:17:53 +0300

    l

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet reversed.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        char alpha;
+
+        for (alpha = 'z'; alpha >= 'a'; alpha--)
+        {
+        putchar(alpha);
+
+        }
+        putchar('\n');
+        return (0);
+}
