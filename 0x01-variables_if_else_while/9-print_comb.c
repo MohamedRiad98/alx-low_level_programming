4bf9dd90569884cd8cf168f6ba9f9959349773b7
root <root@MohammedPC>
2023-06-19 20:59:36 +0300

    l

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 : 9 with , and space after each number.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char num = '0';
+
+	while (num <='9')
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
