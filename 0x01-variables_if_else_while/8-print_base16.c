8d3f62ef2b1444b53293284bc0a15513c54b21f3
root <root@MohammedPC>
2023-06-19 20:34:14 +0300

    l

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,16 +7,17 @@
  */
 int main(void)
 {
-        int num;
+	int num;
 	char alpha;
 
-        for (num = '0'; num <= '9'; num++)
-        {
-        putchar(num);
-        }
-        for(alpha = 'a'; alpha <= 'f'; alpha++){
-            putchar(alpha);
-        }
-        putchar('\n');
-        return (0);
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for(alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
 }
