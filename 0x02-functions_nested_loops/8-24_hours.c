3082d92ec915ed324bc4f3fe8e8c46dd94c67265
root <root@MohammedPC>
2023-06-25 20:52:08 +0300

    l

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * Jack Bauer - prints time table in format of HH:MM
+ * Return The sum of two numbers.
+ */
+
+int main()
+{
+	int h,m;
+	for(h = 0; h < 24;h++)
+	{
+		for(m = 0; m < 60;m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
