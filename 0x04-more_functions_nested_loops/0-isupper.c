57ed6dc8144c802acadb54747ecc9f7ae5d46d2d
Mohamed Riad <mohamedreyad8811@gmail.com>
2023-06-28 13:54:08 -0700

    l

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,22 +1,18 @@
 #include "main.h"
 /**
- * This project will print the output of alphabet in uppercase.
- * @c: int
- * Return 1 if upper 0 if else
+ * _isupper - checks for uppercase character
+ *@c: int
+ *Return: 1 if upper, 0 other
  */
-int main()
+
+int _isupper(int c)
 {
-	int _isupper(int c)
-	{
-		char a
-		for(a = 'A'; a = 'Z'; a++)
-		{
-			if(a == c)
-			{
-				printf("%c is upper charactor", a);
-				return(1);
-			}
-		}
-		return(0);
-	}
+char alpha;
+for (alpha = 'A'; alpha <= 'Z'; alpha++)
+{
+	if (c == alpha)
+		return (1);
+}
+
+return (0);
 }
