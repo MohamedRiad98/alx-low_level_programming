4ff6cab552d332083040424a938a537380c8c13d
MohamedRiad98 <mohamedreyad8811@gmail.com>
2023-09-05 18:18:48 +0300

    l

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,12 +30,12 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (i=0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 
 		new[i] = s1[i];
 	}
-	for (j=0; s2[j] != '\0'; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 	{
 
 		new[i + j] = s2[j];
