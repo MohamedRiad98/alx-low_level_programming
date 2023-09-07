ec0ea0e5fc7d1c177840a663d480c0a6ac511a36
MohamedRiad98 <mohamedreyad8811@gmail.com>
2023-09-08 02:11:55 +0300

    l

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,6 +31,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
 	concat = (char *)malloc(len1 + n + 1 * sizeof(char));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
 	i = 0;
 
 	for (len1 = 0; s1[len1] != '\0';len1++)
