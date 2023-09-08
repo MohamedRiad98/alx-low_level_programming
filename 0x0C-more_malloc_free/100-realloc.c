7576420e7eaf2edd51f36701f6cc51e41c6dd945
MohamedRiad98 <133154356+MohamedRiad98@users.noreply.github.com>
2023-09-08 03:27:02 +0300

    Update 100-realloc.c
    s

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,34 +11,34 @@
 * Return: pointer to the address of the new memory block
 */
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) {
-    void *new_ptr;
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
 
-    /* If new size is zero, free the memory block */
-    if (new_size == 0 && ptr != NULL) {
-        free(ptr);
-        return NULL;
-    }
-    /* If ptr is NULL, allocate a new memory block */
-    if (ptr == NULL) {
-        new_ptr = malloc(new_size);
-        if (new_ptr == NULL) {
-            return NULL;
-        }
-        return new_ptr;
-    }
-    /* If new size is equal to old size, do nothing */
-    if (new_size == old_size) {
-        return ptr;
-    }
-    /* Allocate a new memory block of the requested size */
-    new_ptr = malloc(new_size);
-    if (new_ptr == NULL) {
-        return NULL;
-    }
-    /* Copy the contents of the old memory block to the new one */
-    memcpy(new_ptr, ptr, (old_size < new_size ? old_size : new_size));
-    /* Free the old memory block */
-    free(ptr);
-    return new_ptr;
+	if (new_size == 0 && ptr != NULL) /* free the memory block if new size is zero */
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL) /* allocate a new memory block if ptr is NULL */
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		return (new_ptr);
+	}
+
+	if (new_size == old_size) /* do nothing if new size is equal to old size */
+		return (ptr);
+
+	new_ptr = malloc(new_size); /* allocate a new memory block of the requested size */
+	if (new_ptr == NULL)
+		return (NULL);
+
+	/* copy the contents of the old memory block to the new one */
+	memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
+
+	free(ptr); /* free the old memory block */
+	return (new_ptr);
 }
