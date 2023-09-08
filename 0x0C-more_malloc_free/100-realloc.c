a79ba35f652fc16fa68d3a9756ea854d7852668c
MohamedRiad98 <133154356+MohamedRiad98@users.noreply.github.com>
2023-09-08 03:32:28 +0300

    Update 100-realloc.c
    a

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
 * _realloc - reallocates a memory block
@@ -12,33 +13,31 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *temp_block;
-	unsigned int i;
+	void *new_ptr;
 
-	if (ptr == NULL)
-	{
-		temp_block = malloc(new_size);
-		return (temp_block);
-	}
-	else if (new_size == old_size)
-		return (ptr);
-
-	else if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else
+
+	if (ptr == NULL)
 	{
-		temp_block = malloc(new_size);
-		if (temp_block != NULL)
-		{
-			for (i = 0; i < min(old_size, new_size); i++)
-				*((char *)temp_block + i) = *((char *) ptr + i);
-			free(ptr);
-			return (temp_block);
-		}
-		else
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
 			return (NULL);
+		return (new_ptr);
 	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+
+	memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
+
+	free(ptr);
+	return (new_ptr);
 }
