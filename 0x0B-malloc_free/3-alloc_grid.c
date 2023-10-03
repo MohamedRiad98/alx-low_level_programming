7a68e84f9200c1c0a84e47bef907f1e3a3f23e19
MohamedRiad98 <mohamedreyad8811@gmail.com>
2023-10-03 23:33:33 +0300

    l

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Entry point
+ *@width: rows of matrix
+ *@height: columns of string
+ * Return: a pointer to a 2 dimensional array of integers or null
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = (int **)malloc(height * sizeof(int *));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(width * sizeof(int));
+		if (array[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+			{
+				free(array[i]);
+				free(array);
+				return (NULL);
+			}
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+	return (array);
+}
