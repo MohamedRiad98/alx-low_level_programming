b95d5168587a2beaa0c26ce77e5154d8b7458f67
Muhammed Riad <mohamedreyad8811@gmail.com>
2024-01-04 20:27:26 +0200

    betty style update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,49 @@
 #include "main.h"
 
+/**
+ * print_times_table - Prints the multiplication table of a given size.
+ * @n: The size of the multiplication table (0 <= n <= 15).
+ */
 void print_times_table(int n)
 {
-  if (n >= 0 && n <= 15)
-  {
-    int i, j, product;
+	if (n >= 0 && n <= 15)
+	{
+		int i, j, product;
 
-    for (i = 0; i <= n; i++)
-    {
-      for (j = 0; j <= n; j++)
-      {
-        product = i * j;
-        
-        if (j == 0)
-          _putchar('0');
-        else if (product <= 9)
-        {
-          _putchar(' ');
-          _putchar(' ');
-          _putchar(product + '0');
-        }
-        else if (product <= 99)
-        {
-          _putchar(' ');
-          _putchar((product / 10) + '0');
-          _putchar((product % 10) + '0');
-        }
-        else
-        {
-          _putchar((product / 100) + '0');
-          _putchar(((product / 10) % 10) + '0');
-          _putchar((product % 10) + '0');
-        }
-        
-        if (j != n)
-        {
-          _putchar(',');
-          _putchar(' ');
-        }
-      }
-      _putchar('\n');
-    }
-  }
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				product = i * j;
+
+				if (j == 0)
+					_putchar('0');
+				else if (product <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				else if (product <= 99)
+				{
+					_putchar(' ');
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+				else
+				{
+					_putchar((product / 100) + '0');
+					_putchar(((product / 10) % 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+
+				if (j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
