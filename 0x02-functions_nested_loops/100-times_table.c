c0e150635b8b5f2b28945d732b6c4310f5a4a455
Muhammed Riad <mohamedreyad8811@gmail.com>
2024-01-04 19:46:24 +0200

    Advanced Project

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
-void print_times_table(int n);
 void print_times_table(int n)
 {
     int i, j, product;
     
-    for (i = 0; i <= n; i++) {
-        for (j = 0; j <= n; j++) {
-            product = i * j;
-            printf("%-4d", product);
-        }
-        
+    if (n > 15 || n < 0)
+    {
+    	for (i = 0; i <= n; i++)
+    	{
+        	for (j = 0; j <= n; j++)
+		{
+            		product = i * j;
+            		printf("%-4d", product);
+        	}
         printf("\n");
+    	}
     }
+    else
+	    return (0);
 }
