8f4a96f7c53dd068ef6a08448b299b9e418017ee
Muhammed Riad <mohamedreyad8811@gmail.com>
2024-01-04 20:20:22 +0200

    l

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,19 +1,45 @@
-#include <stdio.h>
+#include "main.h"
 
 void print_times_table(int n)
 {
+  if (n >= 0 && n <= 15)
+  {
     int i, j, product;
-    
-    if (n > 15 || n < 0)
+
+    for (i = 0; i <= n; i++)
     {
-    	for (i = 0; i <= n; i++)
-    	{
-        	for (j = 0; j <= n; j++)
-		{
-            		product = i * j;
-            		printf("%-4d", product);
-        	}
-        printf("\n");
-    	}
+      for (j = 0; j <= n; j++)
+      {
+        product = i * j;
+        
+        if (j == 0)
+          _putchar('0');
+        else if (product <= 9)
+        {
+          _putchar(' ');
+          _putchar(' ');
+          _putchar(product + '0');
+        }
+        else if (product <= 99)
+        {
+          _putchar(' ');
+          _putchar((product / 10) + '0');
+          _putchar((product % 10) + '0');
+        }
+        else
+        {
+          _putchar((product / 100) + '0');
+          _putchar(((product / 10) % 10) + '0');
+          _putchar((product % 10) + '0');
+        }
+        
+        if (j != n)
+        {
+          _putchar(',');
+          _putchar(' ');
+        }
+      }
+      _putchar('\n');
     }
+  }
 }
